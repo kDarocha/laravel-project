dd55ac9f70b197e5af8dea99382add42486d9897
Jojotartiflette <jolan.maggiochi@epitech.eu>
2018-09-16 14:02:28 +0200

    Create database migration + seeders

---
+++ database/migrations/2018_09_16_105901_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title_fr');
+            $table->string('title_en');
+            $table->string('description_fr');
+            $table->string('description_en');
+            $table->string('content_fr');
+            $table->string('content_en');
+            $table->string('slug');
+            $table->string('picture');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
