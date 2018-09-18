f774b29d38c8d4fae7a82d5dc4d97a9cd563b07e
Kevin DA ROCHA <kev.darocha@gmail.com>
2018-09-18 16:16:33 +0200

    Add wizywig on about textarea

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -5,4 +5,18 @@
         <label for="about-content">Contenu</label>
         <textarea name="about-content" id="about-content" cols="30" rows="10" class="form-control">{{ $about->content }}</textarea>
     </div>
+    <div class="form-group">
+        <input id="main-picture" name="name-picture" type="file" class="form-control">
+    </div>
+    <div class="form-group">
+        <img width="35%" src="{{ asset('images/about.jpg') }}" alt="">
+    </div>
+@endsection
+
+@section('script')
+    <script>
+      $(function() {
+        $('textarea#about-content').froalaEditor();
+      });
+    </script>
 @endsection
