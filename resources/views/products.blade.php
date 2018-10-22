b4d2baceca5400f9427f9017984877cec5266b97
Kevin DA ROCHA <kev.darocha@gmail.com>
2018-10-22 09:47:19 +0200

    Fix style on product page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="latest-news-content">
                 <div class="row" style="display: flex;  flex-wrap: wrap;">
                     @foreach($products as $product)
